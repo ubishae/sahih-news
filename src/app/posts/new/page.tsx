9dce896d5f6ea31cab562ac394ac35668cbddce5
Ahmed Bashir <bashir@0g.ae>
2025-04-11 13:02:31 +0400

    Add new post creation page with rich text editor and form submission

--- src/app/posts/new/page.tsx
+++ src/app/posts/new/page.tsx
@@ -4,6 +4,7 @@ import RichTextEditor from "@/components/rich-text-editor";
 import { Button } from "@/components/ui/button";
 import { api } from "@/trpc/react";
 import { useForm } from "@tanstack/react-form";
+import { Loader } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
@@ -53,15 +54,34 @@ export default function NewPostPage() {
 				)}
 			/>
 
-			<div className="mt-5 flex justify-between">
-				<div>
-					<Button variant="outline">Cancel</Button>
-				</div>
-				<div className="flex gap-5">
-					<Button variant="secondary">Save Draft</Button>
-					<Button type="submit">Publish</Button>
-				</div>
-			</div>
+			<form.Subscribe
+				selector={(state) => [state.canSubmit, state.isSubmitting]}
+				children={([canSubmit, isSubmitting]) => (
+					<div className="mt-5 flex justify-between">
+						<div>
+							<Button variant="outline" disabled={isSubmitting}>
+								Cancel
+							</Button>
+						</div>
+						<div className="flex gap-5">
+							<Button variant="secondary" disabled={!canSubmit || isSubmitting}>
+								{isSubmitting ? (
+									<Loader className="h-4 w-4 animate-spin" />
+								) : (
+									"Save Draft"
+								)}
+							</Button>
+							<Button type="submit" disabled={!canSubmit || isSubmitting}>
+								{isSubmitting ? (
+									<Loader className="h-4 w-4 animate-spin" />
+								) : (
+									"Publish"
+								)}
+							</Button>
+						</div>
+					</div>
+				)}
+			/>
 		</form>
 	);
 }
