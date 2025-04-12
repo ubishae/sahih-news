5f4d659de10d7e552116c2bea66d175668f7d587
bisha <bisha.mail.drop@gmail.com>
2025-04-12 15:37:29 +0400

    Refactor vote logic to use 'and' operator for query conditions in voteRouter, improving clarity and consistency in vote management.

--- src/server/api/routers/vote.ts
+++ src/server/api/routers/vote.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { posts, votes } from "@/server/db/schema";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { TRPCError } from "@trpc/server";
 
 export const voteRouter = createTRPCRouter({
@@ -22,7 +22,7 @@ export const voteRouter = createTRPCRouter({
 			// Check if the user has already voted on this post
 			const existingVote = await ctx.db.query.votes.findFirst({
 				where: (vote) =>
-					eq(vote.userId, ctx.id) && eq(vote.postId, input.postId),
+					and(eq(vote.userId, ctx.id), eq(vote.postId, input.postId)),
 			});
 
 			if (existingVote) {
@@ -30,13 +30,17 @@ export const voteRouter = createTRPCRouter({
 				if (existingVote.type === "upvote") {
 					await ctx.db
 						.delete(votes)
-						.where(eq(votes.userId, ctx.id) && eq(votes.postId, input.postId));
+						.where(
+							and(eq(votes.userId, ctx.id), eq(votes.postId, input.postId)),
+						);
 				} else {
 					// Otherwise update the vote to upvote
 					await ctx.db
 						.update(votes)
 						.set({ type: "upvote" })
-						.where(eq(votes.userId, ctx.id) && eq(votes.postId, input.postId));
+						.where(
+							and(eq(votes.userId, ctx.id), eq(votes.postId, input.postId)),
+						);
 				}
 			} else {
 				// Insert a new vote
@@ -47,6 +51,7 @@ export const voteRouter = createTRPCRouter({
 				});
 			}
 		}),
+
 	downvote: protectedProcedure
 		.input(z.object({ postId: z.number() }))
 		.mutation(async ({ ctx, input }) => {
@@ -64,7 +69,7 @@ export const voteRouter = createTRPCRouter({
 			// Check if the user has already voted on this post
 			const existingVote = await ctx.db.query.votes.findFirst({
 				where: (vote) =>
-					eq(vote.userId, ctx.id) && eq(vote.postId, input.postId),
+					and(eq(vote.userId, ctx.id), eq(vote.postId, input.postId)),
 			});
 
 			if (existingVote) {
@@ -72,13 +77,17 @@ export const voteRouter = createTRPCRouter({
 				if (existingVote.type === "downvote") {
 					await ctx.db
 						.delete(votes)
-						.where(eq(votes.userId, ctx.id) && eq(votes.postId, input.postId));
+						.where(
+							and(eq(votes.userId, ctx.id), eq(votes.postId, input.postId)),
+						);
 				} else {
 					// Otherwise update the vote to downvote
 					await ctx.db
 						.update(votes)
 						.set({ type: "downvote" })
-						.where(eq(votes.userId, ctx.id) && eq(votes.postId, input.postId));
+						.where(
+							and(eq(votes.userId, ctx.id), eq(votes.postId, input.postId)),
+						);
 				}
 			} else {
 				// Insert a new vote
